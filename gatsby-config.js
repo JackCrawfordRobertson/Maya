a16d81af356caeb7951ebe186b274b6d3ceb16b1
Jack Robetson <jackrobertsonps3@gmail.com>
2024-01-22 19:01:21 +0000

    SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     "gatsby-plugin-react-helmet", 
     "gatsby-plugin-image",
     "gatsby-plugin-sitemap",
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {

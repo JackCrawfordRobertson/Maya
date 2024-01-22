3489d645e0fae90154b7d36df9bc8d22f8413f68
Jack Robetson <jackrobertsonps3@gmail.com>
2024-01-22 18:37:55 +0000

    SEO and Naming updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,27 +3,55 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Water Synthesis North Bekka`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Maya - Accessible Climate Knowledge`,
+    siteUrl: `https://maya.jack-robertson.co.uk/`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+  plugins: [
+    "gatsby-plugin-react-helmet", 
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-78CFC63TFD", 
+        ],
+      
+        gtagConfig: {
+          anonymize_ip: true, 
+          cookie_expires: 0, 
+        },
+        pluginConfig: {
+          head: true,
+          respectDNT: true, 
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     }
-  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-//index.js
 import React from "react";
+import {Helmet} from "react-helmet";
 import App from "./MainConsolidation/App.js";
 import "./global.css";
 import "mapbox-gl/dist/mapbox-gl.css";
@@ -19,4 +19,16 @@ const IndexPage = () => {
 
 export default IndexPage;
 
-export const Head = () => <title>Maya</title>;
+export const Head = () => (
+    <Helmet>
+        <title>Maya - Accessible Climate Knowledge</title>
+        <meta
+            name="description"
+            content="Maya is a peer-reviewed platform designed to bridge the gap between specialists and non-specialists in the climate field. It offers a unique space where specialist data is synthesized and presented in an accessible, informative manner. Our goal is to empower non-specialists with reliable and understandable climate knowledge, facilitating informed decisions and greater awareness. Maya leverages advanced data visualization techniques and collaborative tools to make complex environmental information easily digestible for everyone."
+        />
+        <meta
+            name="keywords"
+            content="gatsby, climate communication, knowledge sharing, peer-reviewed content, specialist insights, environmental data, accessible information, climate change education, sustainable practices, community engagement, climate science, data visualization, environmental awareness, interdisciplinary collaboration, climate policy, environmental research"
+        />
+    </Helmet>
+);

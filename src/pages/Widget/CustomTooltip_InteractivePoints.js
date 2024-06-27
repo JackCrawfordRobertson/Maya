d03725e5fe337331ad080af31eef3580f21cb7ec
Jack Robetson <jackrobertsonps3@gmail.com>
2024-06-27 15:04:45 +0100

    Deful Export fix for CoustomTooltip

--- src/pages/Widget/CustomTooltip_InteractivePoints.js
+++ src/pages/Widget/CustomTooltip_InteractivePoints.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const CustomTooltip = ({ point }) => {
-    return (
-        <div style={{ padding: '5px', background: 'white', border: '1px solid #ccc' }}>
-            <strong>Water Produced:</strong> {point.data.yFormatted}
-        </div>
-    );
+  if (!point || !point.data) {
+    // Handle the case where point or point.data is undefined
+    return <div style={{ padding: '5px', background: 'white', border: '1px solid #ccc' }}>Loading...</div>;
+  }
+
+  return (
+    <div style={{ padding: '5px', background: 'white', border: '1px solid #ccc' }}>
+      <strong>Water Produced:</strong> {point.data.yFormatted}
+    </div>
+  );
 };
+
+export default CustomTooltip;
